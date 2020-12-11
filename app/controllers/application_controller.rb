d33afb25cf91299acee82e54b222c6efb1804ae4
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-11 19:32:05 +0900

    新規登録情報の許可

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :full_firstname, :full_lastname, :kana_firstname, :kana_lastname, :birth_date])
+  end
 end
