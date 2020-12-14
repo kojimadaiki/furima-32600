976c50db84bcc7cf6d148d96bd71065ba7a49971
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-14 21:22:48 +0900

    statusドロップダウンの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,10 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :status
 
   validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+
 
 end
