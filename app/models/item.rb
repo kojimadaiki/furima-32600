a5563b0c14527225752a9e07ef2c3cbb2840fb4d
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-14 21:27:43 +0900

    deliveryドロップダウンの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,9 +16,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :delivery
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
 
 
 end
