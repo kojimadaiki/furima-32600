a1caaada2a07918f080fdcf1c27ecdffbed72a2c
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-14 17:53:35 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates: name, length: { maxmum: 40 }
+    validates: explain, length: { maxmum: 1000 }
+    validates: category_id
+    validates: status_id
+    validates: delivery_fee_id
+    validates: prefecture_id
+    validates: shipping_date_id
+    validates: price, format: { with: /\A[0-9]+\z/, message: '' }
+
 end
