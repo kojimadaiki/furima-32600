05125a94f3c6c19686998e8b05a5c4c784ca678c
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-14 12:50:46 +0900

    rubocop使用

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-  validates :name
-  validates :full_lastname, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width chinese characters' }
-  validates :full_firstname, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width chinese characters' }
-  validates :kana_lastname, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
-  validates :kana_firstname, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
-  validates :birth_date
-end
+    validates :name
+    validates :full_lastname, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width chinese characters' }
+    validates :full_firstname, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width chinese characters' }
+    validates :kana_lastname, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
+    validates :kana_firstname, format: { with: /\p{katakana}/, message: 'Full-width katakana characters' }
+    validates :birth_date
+  end
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    name                  {Faker::Name.initials}
-    email                 {Faker::Internet.free_email}
-    password              {"aaa1aaa"}
-    password_confirmation {password}
-    full_firstname        {"山田"}
-    full_lastname         {"太郎"}
-    kana_firstname        {"ヤマダ"}
-    kana_lastname         {"タロウ"}
-    birth_date            {"2000-01-01"}
+    name                  { Faker::Name.initials }
+    email                 { Faker::Internet.free_email }
+    password              { 'aaa1aaa' }
+    password_confirmation { password }
+    full_firstname        { '山田' }
+    full_lastname         { '太郎' }
+    kana_firstname        { 'ヤマダ' }
+    kana_lastname         { 'タロウ' }
+    birth_date            { '2000-01-01' }
   end
 end
