640bf7cc7b0f989024d47ca8cfaf4c16d4ef589b
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-14 17:53:14 +0900

    itemデータ保存のルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,19 @@
 class ItemsController < ApplicationController
-  
+
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explain, :category_id, :status_id, :delivery_fee_id, :prefecture_id, :shipping_date_id, :price)
   end
 
 end
