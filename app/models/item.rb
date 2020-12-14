55b0a71b0ac1eb75e6dac3eb4022114652e2c830
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-14 18:10:41 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
 
   with_options presence: true do
-    validates: name, length: { maxmum: 40 }
-    validates: explain, length: { maxmum: 1000 }
-    validates: category_id
-    validates: status_id
-    validates: delivery_fee_id
-    validates: prefecture_id
-    validates: shipping_date_id
-    validates: price, format: { with: /\A[0-9]+\z/, message: '' }
+    validates :name, length: { maximum: 40 }
+    validates :explain, length: { maximum: 1000 }
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price, format: { with: /\A[0-9]+\z/, message: '' }
+  end
 
     has_one_attached :image
 
