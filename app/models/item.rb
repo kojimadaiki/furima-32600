722869f1d861de6b210b136690a43ed47da8f8b8
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-15 10:51:39 +0900

    prefectureドロップダウンの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,10 +17,11 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :delivery
+  belongs_to :prefecture
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
   validates :delivery_fee_id, numericality: { other_than: 1 } 
-
+  validates :prefecture_id, numericality: { other_than: 1 } 
 
 end
