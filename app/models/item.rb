1410d70d45cb85002a51c9c3c1354ebf8bd4241e
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-15 17:15:39 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,13 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality: {only_integer: true}
+    validates :price, numericality: { only_integer: true, message: "is not number" }
   end
-
+  
   has_one_attached :image
   validate :image_presence
   belongs_to :user
-
+  
   def image_presence
     if image.attached?
       if !image.content_type.in?(%('image/jpeg image/png'))
@@ -24,18 +24,19 @@ class Item < ApplicationRecord
       errors.add(:image, "can't be blank")
     end
   end
-
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :delivery
   belongs_to :prefecture
   belongs_to :shipping
-
+  
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
   validates :delivery_fee_id, numericality: { other_than: 1 } 
   validates :prefecture_id, numericality: { other_than: 1 } 
   validates :shipping_date_id, numericality: { other_than: 1 } 
-
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is from 300yen to 9999999yen'}
+  
 end
