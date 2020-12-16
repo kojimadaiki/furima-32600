ab40c3a5b67612f65d4c06dba25dc0ff5803f188
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-16 20:59:59 +0900

    レビュー依頼後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -35,7 +37,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path if @item.destroy
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
