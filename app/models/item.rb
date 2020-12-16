80ecf9dbb7aa514a3e04415965904b6f6c44fa9a
kojimadaiki <swim1205@ezweb.ne.jp>
2020-12-16 12:55:03 +0900

    レビュー依頼後修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,7 +25,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
-  belongs_to :delivery
+  belongs_to :delivery_fee
   belongs_to :prefecture
   belongs_to :shipping
 
